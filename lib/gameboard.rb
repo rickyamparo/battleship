515f4e6530213ceced9f5d35799d4a553f39ba34
RickyAmparo <rickyamparo@gmail.com>
2017-07-15 22:12:58 -0600

    Create generate rows method for gameboard class

--- lib/gameboard.rb
+++ lib/gameboard.rb
@@ -3,11 +3,22 @@ require './lib/space'
 
 class Gameboard
 
+  attr_reader :row_a, :row_b, :row_c, :row_d
+
   def initialize
     @row_a = []
     @row_b = []
     @row_c = []
     @row_d = []
   end
+
+  def generate_rows
+    @row_a = [Space.new, Space.new, Space.new, Space.new]
+    @row_b = [Space.new, Space.new, Space.new, Space.new]
+    @row_c = [Space.new, Space.new, Space.new, Space.new]
+    @row_d = [Space.new, Space.new, Space.new, Space.new] 
+  end
+
+
 
 end
--- test/gameboard_test.rb
+++ test/gameboard_test.rb
@@ -12,4 +12,30 @@ class Gameboard_test < Minitest::Test
     assert_instance_of Gameboard, Gameboard.new
   end
 
+  def test_gameboard_has_rows
+    gb = Gameboard.new
+    assert_instance_of Array, gb.row_a
+    assert_instance_of Array, gb.row_b
+    assert_instance_of Array, gb.row_c
+    assert_instance_of Array, gb.row_d
+  end
+
+  def test_gameboard_can_create_rows
+    gb = Gameboard.new
+    gb.generate_rows
+    assert_instance_of Array, gb.row_a
+    assert_instance_of Array, gb.row_b
+    assert_instance_of Array, gb.row_c
+    assert_instance_of Array, gb.row_d
+  end
+
+  def test_gameboard_rows_are_made_of_spaces
+    gb = Gameboard.new
+    gb.generate_rows
+    assert_instance_of Space, gb.row_a[0]
+    assert_instance_of Space, gb.row_a[1]
+    assert_instance_of Space, gb.row_a[2]
+    assert_instance_of Space, gb.row_a[3]
+  end
+
 end
