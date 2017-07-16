0da2e7ab49aa8130276149c8dc0418c62f023541
RickyAmparo <rickyamparo@gmail.com>
2017-07-16 13:05:26 -0600

    Merge gamboard_printer functionality into gameboard class, merge test, updating print_whole_board method

--- lib/gameboard.rb
+++ lib/gameboard.rb
@@ -6,20 +6,50 @@ class Gameboard
   attr_reader :row_a, :row_b, :row_c, :row_d
 
   def initialize
-    @row_a = []
-    @row_b = []
-    @row_c = []
-    @row_d = []
-    generate_rows
-  end
-
-  def generate_rows
     @row_a = [Space.new, Space.new, Space.new, Space.new]
     @row_b = [Space.new, Space.new, Space.new, Space.new]
     @row_c = [Space.new, Space.new, Space.new, Space.new]
     @row_d = [Space.new, Space.new, Space.new, Space.new]
   end
 
+  def print_whole_board
+    p print_row_a + "\n"
+    p print_row_b + "\n"
+    p print_row_c + "\n"
+    p print_row_d + "\n"
+  end
+
+  def print_row_a
+    current_row = ""
+    @row_a.each do |space|
+      current_row += space.status
+    end
+    current_row
+  end
+
+  def print_row_b
+    current_row = ""
+    @row_b.each do |space|
+      current_row += space.status
+    end
+    current_row
+  end
+
+  def print_row_c
+    current_row = ""
+    @row_c.each do |space|
+      current_row += space.status
+    end
+    current_row
+  end
+
+  def print_row_d
+    current_row = ""
+    @row_d.each do |space|
+      current_row += space.status
+    end
+    current_row
+  end
 
 
 end
--- test/gameboard_test.rb
+++ test/gameboard_test.rb
@@ -22,7 +22,6 @@ class Gameboard_test < Minitest::Test
 
   def test_gameboard_can_create_rows
     gb = Gameboard.new
-    gb.generate_rows
     assert_instance_of Array, gb.row_a
     assert_instance_of Array, gb.row_b
     assert_instance_of Array, gb.row_c
@@ -31,11 +30,24 @@ class Gameboard_test < Minitest::Test
 
   def test_gameboard_rows_are_made_of_spaces
     gb = Gameboard.new
-    gb.generate_rows
     assert_instance_of Space, gb.row_a[0]
     assert_instance_of Space, gb.row_a[1]
     assert_instance_of Space, gb.row_a[2]
     assert_instance_of Space, gb.row_a[3]
+  end
+
+  def test_it_can_print_each_rows
+    gb = Gameboard.new
+    assert_equal "            ", gb.print_row_a
+    assert_equal "            ", gb.print_row_b
+    assert_equal "            ", gb.print_row_c
+    assert_equal "            ", gb.print_row_d
+  end
+
+  def test_gameboard_can_print_whole_board
+    skip
+    gb = Gameboard.new
+    assert_equal "", gb.print_whole_board
   end
 
 end
