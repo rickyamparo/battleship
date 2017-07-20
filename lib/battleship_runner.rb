4a63022b9fb30ce0bd3382d092585ffa501f5319
RickyAmparo <rickyamparo@gmail.com>
2017-07-20 10:25:10 -0600

    Fix input error during ship placement, created a new input_validator method and removed that functionality from spot validators

--- lib/battleship_runner.rb
+++ lib/battleship_runner.rb
@@ -2,10 +2,11 @@ require 'pry'
 require './lib/battleship'
 
 bs = Battleship.new
-
+turns = 0
 while bs.player != nil
   bs.player_turn
   bs.computer_turn
+  turns += 1
   if bs.hit_check(bs.player.player_board.board) == 5
     print "The enemy won the game!!!"
     break
@@ -14,3 +15,4 @@ while bs.player != nil
     break
   end
 end
+puts "\nThis game took " + turns.to_s + " turns"
--- lib/player.rb
+++ lib/player.rb
@@ -11,27 +11,27 @@ class Player
   end
 
   def ship_sequence
-    puts "Where would you like your first ship's, first spot placed?"
     first_ship_placement
-    puts "Where would you like your second ship's, first spot placed?"
     second_ship_placement
   end
 
   def first_ship_placement
-    first_spot = gets.chomp
+    puts "Where would you like your first ship's, first spot placed?"
+    first_spot = input_validator
     puts "What's your first ship's last spot?"
-    second_spot = gets.chomp
+    second_spot = input_validator
+    first_validator(first_spot, second_spot)
     first_spot_placement(first_spot)
     second_spot_placement(second_spot)
-    first_validator(first_spot, second_spot)
   end
 
   def second_ship_placement
-    first_spot = gets.chomp
-    puts "what's your second spot?"
-    second_spot = gets.chomp
-    puts "what's your last spot?"
-    third_spot = gets.chomp
+    puts "Where would you like your second ship's, first spot placed?"
+    first_spot = input_validator
+    puts "What's your second ship's second spot?"
+    second_spot = input_validator
+    puts "What's your second ship's last spot?"
+    third_spot = input_validator
     second_validator(first_spot, second_spot)
     second_validator(second_spot, third_spot)
     third_spot_validator(first_spot, third_spot)
@@ -41,12 +41,21 @@ class Player
     puts "Those spaces are valid."
   end
 
+  def input_validator
+    spot = gets.chomp
+    while @player_board.idmap[spot] == nil
+      puts "Please input a valid coordinate."
+      spot = gets.chomp
+    end
+    spot
+  end
+
   def first_spot_placement(first_spot)
     if @player_board.idmap[first_spot] != nil
       @player_board.board[@player_board.idmap[first_spot]].ship_placement
     else
-      puts "That is not an appropriate space"
-      first_spot_placement(first_spot)
+      puts "That is not an appropriate space, please try again.1"
+      first_ship_placement
     end
   end
 
@@ -54,8 +63,8 @@ class Player
     if @player_board.idmap[second_spot] != nil
       @player_board.board[@player_board.idmap[second_spot]].ship_placement
     else
-      puts "That is not an appropriate space"
-      second_spot_placement(second_spot)
+      puts "That is not an appropriate space, please try again.2"
+      first_ship_placement
     end
   end
 
@@ -63,8 +72,8 @@ class Player
     if @player_board.idmap[third_spot] != nil
       @player_board.board[@player_board.idmap[third_spot]].ship_placement
     else
-      puts "That is not an appropriate space"
-      third_spot_placement(third_spot)
+      puts "That is not an appropriate space, please try again.3"
+      second_ship_placement
     end
   end
 
@@ -74,12 +83,12 @@ class Player
           @player_board.idmap[first_spot] == @player_board.idmap[second_spot] - 4 ||
             @player_board.idmap[first_spot] == @player_board.idmap[second_spot] + 4)
     else
-      puts "Those spaces are not valid, please try again."
+      puts "Those spaces are not valid, please try again.4"
       first_ship_placement
     end
     if @player_board.idmap[first_spot] % 4 == 3 && @player_board.idmap[second_spot] % 4 == 0 ||
       @player_board.idmap[second_spot] % 4 == 3 && @player_board.idmap[first_spot] % 4 == 0
-      puts "Those spaces are not valid, please try again"
+      puts "Those spaces are not valid, please try again.5"
       first_ship_placement
     end
   end
@@ -91,16 +100,16 @@ class Player
             @player_board.idmap[first_spot] == @player_board.idmap[second_spot] - 4 ||
               @player_board.idmap[first_spot] == @player_board.idmap[second_spot] + 4)
       else
-        puts "Those spaces are not valid, please try again."
+        puts "Those spaces are not valid, please try again. 7"
         second_ship_placement
       end
       if (@player_board.idmap[first_spot] % 4 == 3 && @player_board.idmap[second_spot] % 4 == 0 ||
           @player_board.idmap[second_spot] % 4 == 3 && @player_board.idmap[first_spot] % 4 == 0)
-          puts "Those spaces are not valid, please try again."
+          puts "Those spaces are not valid, please try again. 8"
           second_ship_placement
       end
     else
-      puts "Those spaces are not valid, please try again."
+      puts "Those spaces are not valid, please try again.9"
       second_ship_placement
     end
   end
@@ -112,16 +121,16 @@ class Player
             @player_board.idmap[first_spot] == @player_board.idmap[third_spot] - 8 ||
               @player_board.idmap[first_spot] == @player_board.idmap[third_spot] + 8)
       else
-        puts "Those spaces are not valid, please try again."
+        puts "Those spaces are not valid, please try again. 11"
         second_ship_placement
       end
       if (@player_board.idmap[first_spot] % 4 == 3 && @player_board.idmap[third_spot] % 4 == 1 ||
           @player_board.idmap[third_spot] % 4 == 0 && @player_board.idmap[first_spot] % 4 == 2)
-          puts "Those spaces are not valid, please try again."
+          puts "Those spaces are not valid, please try again. 12"
           second_ship_placement
       end
     else
-      puts "Those spaces are not valid, please try again."
+      puts "Those spaces are not valid, please try again. 13"
       second_ship_placement
     end
   end
